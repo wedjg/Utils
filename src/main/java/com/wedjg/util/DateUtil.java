554bba4e7ecc772403f760406504e6643e5250bb
liaojiajian <jjliao@cesz.com>
2019-05-17 09:23:09 +0800

    feat: 增加了时间工具类的方法

--- src/main/java/com/wedjg/util/DateUtil.java
+++ src/main/java/com/wedjg/util/DateUtil.java
@@ -14,6 +14,27 @@ public final class DateUtil {
     public static final String MONTH_PATTERN = "yyyy-MM";
     public static final String YEAR_PATTERN = "yyyy";
     public static final String DATE_PATTERN2 = "yyyy/MM/dd";
+	
+	/**
+	 *	获取当前标准格式的时间yyyy-MM-dd HH:mm:ss
+	 */
+	public static String getStandardTime() {
+		return this.formatTime(new Date(), DATE_TIME_PATTERN);
+	}
+	
+	/**
+	 *	获取当前时间
+	 */
+	public static String getTimeByPattern(String pattern) {
+		return this.formatTime(new Date(), pattern);
+	}
+	
+	/**
+	 *	格式化时间
+	 */
+	public static String formatTime(Date date, String pattern) {
+		return new SimpleDateFormat(pattern).format(date);
+	}
 
 
 }
